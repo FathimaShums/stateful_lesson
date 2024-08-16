70952df101aa7a258de92c76b6439e26b1a30ba8
FathimaShums <fathimashums416@gmail.com>
2024-08-16 12:42:26 +0530

    actually fixed issue with spanning

--- lib/babycolumone.dart
+++ lib/babycolumone.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:stateful_lesson/forminfo.dart';
+import 'package:stateful_lesson/myHome.dart';
 
 class Babycolumone extends StatelessWidget {
   const Babycolumone({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        forminfo(),
+        MyHomeScreen(thehinttext: "Kavin", theTitle: "Name")
+      ],
+    ));
   }
 }
--- lib/myHome.dart
+++ lib/myHome.dart
@@ -23,96 +23,100 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
     return Scaffold(
         body: Form(
       key: _loginFormKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              widget.theTitle,
-              style: TextStyle(color: Colors.black, fontSize: 16),
+      child: Container(
+        color: Colors.pinkAccent,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                widget.theTitle,
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
             ),
-          ),
-          // Text("Name"),
-          TextFormField(
-            decoration: InputDecoration(
-              hintStyle: TextStyle(color: Colors.black),
-              hintText: widget.thehinttext,
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 14, 12, 14),
-              ),
-              fillColor: Colors.white,
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                borderSide: BorderSide(
-                  width: 2.0,
+            // Text("Name"),
+            TextFormField(
+              decoration: InputDecoration(
+                hintStyle: TextStyle(color: Colors.black),
+                hintText: widget.thehinttext,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 14, 12, 14),
                 ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(
-                  color: Colors.green,
-                  width: 2.0,
-                ),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(
-                  color: Colors.red,
-                  width: 2.0,
-                ),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(
-                  color: Colors.red,
-                  width: 2.0,
-                ),
-              ),
-              errorStyle: TextStyle(color: Colors.redAccent),
-            ),
-            style: TextStyle(color: Colors.black),
-            validator: (val) => val!.isEmpty || val.length != 10
-                ? 'Enter a 10 Digit phone number' //Error Message
-                : null,
-            onChanged: (val) {
-              setState(() {
-                email = val;
-              });
-            },
-          ),
-          Text(x),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                  padding:
-                      WidgetStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
-                  shadowColor: WidgetStateProperty.all<Color>(Colors.grey),
-                  elevation: WidgetStateProperty.resolveWith<double>(
-                    (Set<WidgetState> states) {
-                      if (states.contains(WidgetState.pressed)) return 10;
-                      return 5; // default elevation
-                    },
+                fillColor: Colors.white,
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide(
+                    width: 2.0,
                   ),
-                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 2.0,
                   ),
-                  animationDuration: Duration(milliseconds: 200)),
-              onPressed: () {
-                if (_loginFormKey.currentState!.validate()) {}
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 2.0,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 2.0,
+                  ),
+                ),
+                errorStyle: TextStyle(color: Colors.redAccent),
+              ),
+              style: TextStyle(color: Colors.black),
+              validator: (val) => val!.isEmpty || val.length != 10
+                  ? 'Enter a 10 Digit phone number' //Error Message
+                  : null,
+              onChanged: (val) {
                 setState(() {
-                  x = tx.text;
+                  email = val;
                 });
               },
-              child: Text('Click'),
             ),
-          )
-        ],
+            Text(x),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    padding:
+                        WidgetStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(Colors.green),
+                    shadowColor: WidgetStateProperty.all<Color>(Colors.grey),
+                    elevation: WidgetStateProperty.resolveWith<double>(
+                      (Set<WidgetState> states) {
+                        if (states.contains(WidgetState.pressed)) return 10;
+                        return 5; // default elevation
+                      },
+                    ),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    animationDuration: Duration(milliseconds: 200)),
+                onPressed: () {
+                  if (_loginFormKey.currentState!.validate()) {}
+                  setState(() {
+                    x = tx.text;
+                  });
+                },
+                child: Text('Click'),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
